ddd2957c114c69c21994a23087bd8d2903b8d68b
Deepesh Kulbhushan Sagar <sagardeepesh2@gmail.com>
2022-10-21 21:20:29 +0530

    Update user.js

--- api/model/user.js
+++ api/model/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  username: Number,
+  username: String,
   password: String,
   phoneno: Number,
   email: String,
