e1485cde8d9d7ab18dc835d72d5643e851852c26
AriaHito <116369002+AriaHito@users.noreply.github.com>
2022-10-21 21:25:43 +0530

    changed array split methods index

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(" ")[0];
+        const token = req.headers.authorization.split(" ")[1];
         console.log(token);
         const verify = jwt.verify(token, 'this is dummy text');
         next();
